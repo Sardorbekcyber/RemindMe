1ad4cd62acbec53088f0d97db31a9e5a7a180789
Sardor Narziyev <sardornarziyev020800@gmail.com>
2021-07-11 01:24:45 +0500

    Added ui and implemented navigation component

--- app/src/main/java/com/turbosoft/remindme/ui/MainActivity.kt
+++ app/src/main/java/com/turbosoft/remindme/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.turbosoft.remindme
+package com.turbosoft.remindme.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.turbosoft.remindme.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/turbosoft/remindme/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.turbosoft.remindme.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.turbosoft.remindme.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
